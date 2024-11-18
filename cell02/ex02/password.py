e2f86a9f54fbf0369df1b9fdf73a13e27401ac43
pmipheta <princemiphetattamongkhon@gmail.com>
2024-11-19 01:22:21 +0700

    msg

---
+++ cell02/ex02/password.py
@@ -0,0 +1,7 @@
+password = "Python is awesome"
+InputPassword = input()
+
+if InputPassword == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED.")
---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,7 @@
+print("Enter number")
+
+number = int(input())
+
+for i in range(0,11) :
+    result = number * i
+    print(f"{i} * {number} = {result}")
