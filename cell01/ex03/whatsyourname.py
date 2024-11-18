51acd9b1449a8dfd995e1a8695474c3861feeb40
pmipheta <princemiphetattamongkhon@gmail.com>
2024-11-18 21:32:57 +0700

    hello

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,4 +1,4 @@
 first = input("Hey, what's your first name? : ")
 last = input("And your last name? : ")
 
-print("Well, pleased to meet you,"+ " " +first +" "+ last)
+print("Well, pleased to meet you,"+  " " +first +" "+ last)
