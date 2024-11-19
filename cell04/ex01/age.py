d6aac0c356945729fd948abaed3091b4d8d975b9
pmipheta <princemiphetattamongkhon@gmail.com>
2024-11-19 17:04:24 +0700

    zuyu

---
+++ cell04/ex01/age.py
@@ -0,0 +1,7 @@
+age = input("Please tell me your age: ")
+print(f"You are  currently {age} years old")
+year=10 
+while year<=30 :
+    future_age = int(age) + year
+    print(f"In {year} years, you'll be {future_age} years old.")
+    year+=10
---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+number1 = int(input("Give me the first number: "))
+number2 = int(input("Give me the second number: "))
+print("Thank you")
+result1 = number1 + number2
+result2 = number1 - number2
+result3 = number1 / number2
+result4 = number1 * number2
+print(f"{number1} + {number2} = {result1}")
+print(f"{number1} - {number2} = {result2}")
+print(f"{number1} / {number2} = {result3}")
+print(f"{number1} * {number2} = {result4}")
