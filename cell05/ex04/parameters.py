41163601c2b79733976d10f4a06bf1455ccf7705
pmipheta <princemiphetattamongkhon@gmail.com>
2024-11-20 16:22:06 +0700

    zukub

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -1,7 +1,12 @@
 import sys
 
-# Count parameters, excluding the script name itself
-num_params = len(sys.argv) - 1
-
-# Print the number of parameters
-print("Number of parameters:", num_params)
+def main():
+    # sys.argv includes the script name as the first argument, so we subtract 1 to get the number of parameters.
+    num_params = len(sys.argv) - 1
+    
+    # Print the output
+    print(f"Number of parameters: {num_params}.")
+    
+# Ensure the script runs only when executed directly
+if __name__ == "__main__":
+    main()
