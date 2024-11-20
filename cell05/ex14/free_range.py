6ae8caacd1d23f97a04b7a31b576cd2dd72e0d57
pmipheta <princemiphetattamongkhon@gmail.com>
2024-11-21 02:27:03 +0700

    range

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -4,5 +4,13 @@ import sys
 text = sys.argv
  
 if len(text)==3 :
-    for i in range(text[1],text[2]+1):
-        print(i)
+    input1=int(text[1])
+    input2=int(text[2])
+
+    result=[]
+
+    for i in range(input1,input2+1):
+        result.append(i)
+    print(result)
+else:
+    print("none")
