67d4811beed8179aa34daf1e9b80709754bd6c26
pmipheta <princemiphetattamongkhon@gmail.com>
2024-11-21 18:10:23 +0700

    method

---
+++ cell06/ex04/method_everywhere.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import sys
+
+def shrink(string):
+    print(string[:8])
+
+def enlarge(string):
+    print(strng.ljust(8,'Z'))
+
+if len(sys.argv)<2:
+    print("none")
+else:
+    for text in sys.argv[1:]:
+        if len(text)>8:
+            shrink(text) #มากกว่า 8
+        elif len(text)<8:
+            enlarge(text) #น้อยกว่า 8
+        else:
+            print(text)
